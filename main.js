01293adf1823ea6ecc4e5ebe116c936f36324056
xiexingen <xiexingen@qianxin.com>
2022-12-02 18:33:21 +0800

    feat: 添加icon

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow , nativeImage } = require('electron')
+const { app, BrowserWindow , nativeImage, Tray, Menu } = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -6,7 +6,44 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    skipTaskbar:true,
+    icon: path.join(__dirname, 'assets/icons/icon.png')
   })
+  // 隐藏默认菜单
+  mainWindow.setMenu(null);
+
+  // 触发关闭时触发
+  mainWindow.on('close', (event) => {
+    // 截获 close 默认行为
+    event.preventDefault();
+    // 点击关闭时触发close事件，我们按照之前的思路在关闭时，隐藏窗口，隐藏任务栏窗口
+    mainWindow.hide();
+    mainWindow.setSkipTaskbar(true);
+  });
+
+   // 新建托盘
+   const tray = new Tray(path.join(__dirname, 'assets/icons/tray.ico'));
+   // 托盘名称
+   tray.setToolTip('Easy Spec');
+   // 托盘菜单
+   const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '显示',
+      click: () => { mainWindow.show() }
+    },
+    {
+      label: '退出',
+      click: () => { mainWindow.destroy() }
+    }
+   ]);
+   // 载入托盘菜单
+   tray.setContextMenu(contextMenu);
+   // 双击触发
+   tray.on('double-click', () => {
+      // 双击通知区图标实现应用的显示或隐藏
+      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+      mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true);
+   });
 
   // 加载指定的远程地址
   mainWindow.loadURL('http://47.106.111.143:8080/')
@@ -16,10 +53,6 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
-  // 创建托盘图片
-  const icon = nativeImage.createFromPath('assets/icons/tray.ico')
-  tray = new Tray(icon)
-
   createWindow()
 
   app.on('activate', function () {
